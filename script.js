044f6688c18fc0663dfb01f5fc8915e9c77fcb47
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-17 11:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
 function mincost(arr)
-{ 
-//write your code here
-	  let pq = [];
-   
-        // Adding items to the pQueue
-        for (let i = 0; i < n; i++) {
-            pq.push(arr[i]);
-        }    
-           
-        pq.sort(function(a,b){return a-b;});
-         
-        // Initialize result
-        let res = 0;
-   
-        // While size of priority queue
-        // is more than 1
-        while (pq.length > 1) {
-            // Extract shortest two ropes from pq
-            let first = pq.shift();
-            let second = pq.shift();
-   
-            // Connect the ropes: update result
-            // and insert the new rope to pq
-            res += first + second;
-            pq.push(first + second);
-            pq.sort(function(a,b){return a-b;});
-        }
-   
-        return res;
+
 // return the min cost
+ minHeap = new MinHeap(arr)
+    res = 0
+
+    while (minHeap.size > 1){
+		first = minHeap.pop()
+        second = minHeap.pop()
+
+        res = res + first + second
+        minHeap.push(first + second)
+	}
+       
+
+    return res
   
 }
 
